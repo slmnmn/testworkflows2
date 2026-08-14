513deff93c8f6d5234e8cef41ec447f0638b0136
Simon Alejandro Rincon Rojas <200717186+slmnmn@users.noreply.github.com>
2026-08-14 15:07:45 -0500

    changed h2

--- src/components/HelpBox.jsx
+++ src/components/HelpBox.jsx
@@ -5,7 +5,7 @@ import './HelpBox.css';
 function HelpBox({ title, text }) {
   return (
     <article className="help-box">
-      <h3>{kelly pero que monda cuando vas a pagar esa vaina marica}</h3>
+      <h2>{title}</h2>
       <p>{text}</p>
     </article>
   );
